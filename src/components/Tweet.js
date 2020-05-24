4cd1f19d4248b46178c33cfed9efb041708e6dc1
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-24 14:59:25 +0530

    Added New Tweet Component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {formatTweet, formatDate}  from '../utils/helpers'
 import {TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
+import {handleToggleTweet} from '../actions/tweets'
 
 class Tweet extends Component{
     toParent = (e, id) => {
@@ -11,6 +12,13 @@ class Tweet extends Component{
 
     handleLike = (e) => {
         e.preventDefault()
+
+        const {dispatch, tweet, authedUser} = this.props
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     render(){
