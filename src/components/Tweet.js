4f33370e2b20a6a8f275a5647ad6fbcdfa7048d4
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-24 15:07:08 +0530

    Added NewTweet, and TweetPage components. Added Routing using React-Router-Dom

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux'
 import {formatTweet, formatDate}  from '../utils/helpers'
 import {TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 import {handleToggleTweet} from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component{
     toParent = (e, id) => {
         e.preventDefault()
-
+        this.props.history.push(`/tweet/${id}`)
     }
 
     handleLike = (e) => {
@@ -29,11 +30,11 @@ class Tweet extends Component{
         }
 
         const {
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
         } = tweet
 
         return (
-            <div className="tweet">
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src = {avatar}
                     alt = {`Avatar of ${name}`}
@@ -62,7 +63,7 @@ class Tweet extends Component{
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -79,4 +80,4 @@ function mapStateToProps({authedUser, users, tweets}, {id}){
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet)) 
